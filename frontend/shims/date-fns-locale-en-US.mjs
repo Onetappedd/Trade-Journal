69529c4e63ffd0add6bf6270a52bf01dcf5052b7
Onetapped <73820758+Onetappedd@users.noreply.github.com>
2025-08-13 21:18:47 -0700

    v1.13

--- frontend/shims/date-fns-locale-en-US.mjs
+++ frontend/shims/date-fns-locale-en-US.mjs
@@ -1,3 +1,4 @@
-import enUS from 'date-fns/locale/en-US/index.js'
+// Re-export the v2 locale in the shape react-day-picker expects
+import enUS from 'date-fns/locale/en-US'
 export { enUS }
 export default enUS
